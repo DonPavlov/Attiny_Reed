cc6c3e6750cfffaa604833a26ae3f54cf8fd7ba2
dehein <paul@hein.ninja>
2017-03-24 17:45:56 +0100

    changed this and that

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,65 @@
 #include <Arduino.h>
-#include <avr/sleep.h>
-#include <avr/interrupt.h>
+#include <avr/sleep.h>    // Sleep Modes
+#include <avr/power.h>    // Power management
 
-const int switchPin                     = 3;
-const int statusLED                     = 2;
+// ATtiny85 sleep mode, wake on pin change interrupt demo
+// Author: Nick Gammon
+// Date: 12 October 2013
 
-void setup() {
+// ATMEL ATTINY 25/45/85 / ARDUINO
+//
+//                  +-\/-+
+// Ain0 (D 5) PB5  1|    |8  Vcc
+// Ain3 (D 3) PB3  2|    |7  PB2 (D 2) Ain1
+// Ain2 (D 4) PB4  3|    |6  PB1 (D 1) pwm1
+//            GND  4|    |5  PB0 (D 0) pwm0
+//                  +----+
 
-    pinMode(switchPin, INPUT);
-    digitalWrite(switchPin, HIGH);
-    pinMode(statusLED, OUTPUT);
+const byte LED = 3;  // pin PB3 / PCINT3
+const byte SWITCH = 4; // pin PB4
 
-    // Flash quick sequence so we know setup has started
-    for (int k = 0; k < 10; k = k + 1) {
-        if (k % 2 == 0) {
-            digitalWrite(statusLED, HIGH);
-        }
-        else {
-            digitalWrite(statusLED, LOW);
-        }
-        delay(100);
-        } // for
-    } // setup
+ISR (PCINT0_vect) 
+ {     
+  digitalWrite (LED, HIGH);
+  delay (500); 
+  digitalWrite (LED, LOW);
+  delay (500); 
+  digitalWrite (LED, HIGH);
+  delay (500); 
+  digitalWrite (LED, LOW);
+  delay (500); 
+ }
+ 
+void setup ()
+  {
+  pinMode (LED, OUTPUT);
+  pinMode (SWITCH, INPUT);
+  digitalWrite (SWITCH, HIGH);  // internal pull-up
+  
+  // pin change interrupt (example for D3)
+  PCMSK  |= bit (PCINT4);  // want pin D3 / pin 2
+  GIFR   |= bit (PCIF);    // clear any outstanding interrupts
+  GIMSK  |= bit (PCIE);    // enable pin change interrupts 
+  
+  }  // end of setup
 
-void sleep() {
-
-    GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
-    PCMSK |= _BV(PCINT3);                   // Use PB3 as interrupt pin
-    ADCSRA &= ~_BV(ADEN);                   // ADC off
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
-
-    sleep_enable();                         // Sets the Sleep Enable bit in the MCUCR Register (SE BIT)
-    sei();                                  // Enable interrupts
-    sleep_cpu();                            // sleep
-
-    cli();                                  // Disable interrupts
-    PCMSK &= ~_BV(PCINT3);                  // Turn off PB3 as interrupt pin
-    sleep_disable();                        // Clear SE bit
-    ADCSRA |= _BV(ADEN);                    // ADC on
-
-    sei();                                  // Enable interrupts
-} // sleep
-
-ISR(PCINT0_vect) {
-    // This is called when the interrupt occurs, but I don't need to do anything in it
-}
-
-void loop() {
-    sleep();
-    digitalWrite(statusLED, HIGH);
-    delay(1000);
-    digitalWrite(statusLED, LOW);
-}
+void loop ()
+  {
+  digitalWrite (LED, HIGH);
+  delay (500); 
+  digitalWrite (LED, LOW);
+  delay (500); 
+  goToSleep ();
+  }  // end of loop
+  
+  
+void goToSleep ()
+  {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  ADCSRA = 0;            // turn off ADC
+  power_all_disable ();  // power off ADC, Timer 0 and 1, serial interface
+  sleep_enable();
+  sleep_cpu();                             
+  sleep_disable();   
+  power_all_enable();    // power everything back on
+  }  // end of goToSleep
